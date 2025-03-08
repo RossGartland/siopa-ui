c36ea804881bbe788f1b01b3cf88917f8a1e7c28
Ross Gartland <rgartland117@hotmail.com>
2025-03-08 00:08:09 +0000

    about page created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     { path: '', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent) },
+    { path: 'about', loadComponent: () => import('./pages/about-us/about-us.component').then(m => m.AboutUsComponent) },
     { path: 'login', loadComponent: () => import('./auth/login/login.component').then(m => m.LoginComponent) },
     { path: 'signup', loadComponent: () => import('./auth/signup/signup.component').then(m => m.SignupComponent) },
     { path: 'dashboard', loadComponent: () => import('./pages/dashboard/dashboard.component').then(m => m.DashboardComponent) },
